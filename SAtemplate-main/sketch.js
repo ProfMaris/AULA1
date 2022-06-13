fce2042357678918855363c1f9ec959776a6a4bc
ProfMaris <95358565+ProfMaris@users.noreply.github.com>
2022-06-13 19:54:12 -0300

    Add files via upload

---
+++ SAtemplate-main/sketch.js
@@ -0,0 +1,14 @@
+// declaração de variaveis
+var nome,idade,cidade
+// atribuir valores
+nome ="Prof. Maris";
+idade ="sem idade";
+cidade ="Rio de Janeiro";
+
+console.log("Boa Noite");
+console.log("Agora ficou interessante");
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+
+
